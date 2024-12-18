84af91e1eb4e5ee17dcf0018d2b17a6af169ee62
VasiliyMakogon <makovasja@gmail.com>
2024-12-18 17:14:52 +0200

    fix code

--- homework_15/homework_15.py
+++ homework_15/homework_15.py
@@ -1,46 +1,43 @@
 class Figure:
 
-    def __init__(self, side_a, angle_b, angle_a):
+    def __init__(self, side_a, angle_a, angle_b):
+
         self.side_a = side_a
-        self.angle_b = angle_b
         self.angle_a = angle_a
+        self.angle_b = angle_b
 
     def __setattr__(self, key, value):
 
         if key == 'side_a':
             if value <= 0:
                 raise AttributeError('Side A should be bigger than 0')
-
-        elif key == 'angle_b':
-            if value <= 0 or value >= 179:
-                raise AttributeError('Angle should be bigger than 0 and less than 179')
-
         elif key == 'angle_a':
             if value <= 0 or value >= 179:
                 raise AttributeError('Angle should be bigger than 0 and less than 179')
 
+
         super().__setattr__(key, value)
 
 
+        if key == 'angle_a':
+            object.__setattr__(self, 'angle_b', 180 - value)
+
+        if key == 'angle_b':
+            object.__setattr__(self, 'angle_a', 180 - value)
+
         if hasattr(self, 'angle_a') and hasattr(self, 'angle_b'):
-            self.correct_angle_sum()
+            if self.angle_a + self.angle_b != 180:
+                raise AttributeError('Sum of angles should be 180')
 
-    def correct_angle_sum(self):
+romb1 = Figure(10, 80,110)
+print(f'\nSide A: {romb1.side_a}\nAngle B: {romb1.angle_b}\nAngle A: {romb1.angle_a}')
 
-        if self.angle_a + self.angle_b != 180:
-            if self.angle_a > self.angle_b:
-                self.angle_b = 180 - self.angle_a
-            else:
-                self.angle_a = 180 - self.angle_b
+romb1.angle_a = 60
+print(f'\nSide A: {romb1.side_a}\nAngle B: {romb1.angle_b}\nAngle A: {romb1.angle_a}')
 
-
-romb1 = Figure(10, 120, 80)
-print(f'\nSide A: {romb1.side_a}\nAngle A: {romb1.angle_a}\nAngle B: {romb1.angle_b}')
+romb1.angle_b = 10
+print(f'\nSide A: {romb1.side_a}\nAngle B: {romb1.angle_b}\nAngle A: {romb1.angle_a}')
 
 
-romb1.angle_a = 170
-print(f'\nSide A: {romb1.side_a}\nAngle A: {romb1.angle_a}\nAngle B: {romb1.angle_b}')
 
 
-romb1.angle_b = 90
-print(f'\nSide A: {romb1.side_a}\nAngle A: {romb1.angle_a}\nAngle B: {romb1.angle_b}')
